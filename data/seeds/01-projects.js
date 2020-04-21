17280ad56f7f380bd576b087863c6745eb01a1f3
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-04-20 21:35:18 -0400

    Fixed projects.js

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,24 +1,8 @@
-const express = require('express');
-const action = require('../helpers/actionModel.js');
-
 exports.seed = function(knex, Promise) {
   return knex('projects').insert([
     {
       name: 'Complete Node.js and Express Challenge',
-      description:
-        'Build and Awesome API Using Node.js and Express to Manage Projects and Actions GTD Style!',
+      description: 'Build an Awesome API Using Node.js and Express to Mange Projects and Actions GTD Style!',
     },
   ]);
 };
-
-server.get('/', (req, res) => {
-  res.status(200).json();
-});
-
-server.put('/', (req, res) => {
-  res.status(201).json();
-});
-
-server.post('/', (req, res) => {
-  res.status()
-});
